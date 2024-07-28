e83f4faf409e6347779b277dd5eb162abd8442c5
Harsh <harsharma1210@gmail.com>
2024-07-28 17:13:36 -0400

    Fixed payload and ability to get info from form

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' //root means the service is available throughout the entire application
 })
 export class DataService {
 
