cef26e73c1cd134fb542b177f0756d61e24769e1
Harsh <harsharma1210@gmail.com>
2024-07-31 10:28:34 -0400

    Formated Output JSON and added message for invalid email format

--- src/app/data-view/data-view.component.ts
+++ src/app/data-view/data-view.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { DataTransfer } from '../data.service'
 
 @Component({
   selector: 'app-data-view',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './data-view.component.html',
   styleUrl: './data-view.component.css'
 })
 export class DataViewComponent implements OnInit {
-  data: string = '';
+  data:any;
 
   constructor(private dataTransfer: DataTransfer) {} //Private variable 'dataTransfer' is of type 'DataTransfer'
 
   ngOnInit() {
-      this.data = this.dataTransfer.getData();
-      console.log(this.data);
+      this.data = this.dataTransfer.getData(); //The data from loginPageComponent should be contained within the 'data' variable now
   }
 }
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -22,13 +22,47 @@ export class LoginPageComponent {
 	  tenantCode:"1000248",
 	  sourceType:"CP"
   };
-  response:string = '';
+  response:any;
 
   constructor(private dataService: DataService, private router: Router, private dataTransfer: DataTransfer) {}; //Constructed the injectible
 
-  onSubmit(event: Event){
-    event.preventDefault() //Prevents the default form submission because that forces a total page reload which makes the app slower
+  onSubmit(event: Event)
+  {
+    //Prevents the default form submission because that forces a total page reload which makes the app slower
+    event.preventDefault() 
 
+    //Check to see if the username/email entered is in the form of an email or not using the following regular expression
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+    if(emailRegex.test(this.payload.loginValue)) //If this returns true, then it matches an email format
+    { 
+      
+      //Send the data to the API and get a response
+      this.dataService.sendData(this.payload).subscribe(
+        (data:any) => {
+          
+          //We keep the data as a JSON object so the next compnent can display it with a <pre></pre> tag
+          this.response = data;
+          
+          console.log("We have sucessfully recieved Data from the API");
+          console.log(this.response);
+
+          //Set the injectable data to response and route to the next component
+          this.dataTransfer.setData(this.response);
+          this.router.navigate(['/data-view']);
+        },
+        (error: HttpErrorResponse) => {
+          this.response = 'Error: ' + JSON.stringify(error);
+        }
+      );
+    }
+    else //It does not match an email format so we reload the page
+    {
+      window.location.reload();
+      alert('You did not input an email!');
+    }
+
+    /*Send the data to the API and get a response
     this.dataService.sendData(this.payload).subscribe(
       (data:any) => {
         this.response = 'Success: ' + JSON.stringify(data);
@@ -40,6 +74,6 @@ export class LoginPageComponent {
       (error: HttpErrorResponse) => {
         this.response = 'Error: ' + JSON.stringify(error);
       }
-    );
+    ); */
   }
 }
